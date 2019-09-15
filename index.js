f2e52855867c089ad21d0f55f1f39807744ae3a4
Oren Benmeir <orenbm11@gmail.com>
2019-09-15 12:39:38 -0400

    set up gridfs-stream and storage engine to upload images and method-override for later use

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // -----------------------------------------------------------------------------------------
 // External Dependencies
 // -----------------------------------------------------------------------------------------
-const express    = require('express');
-const app        = express();
-const bodyParser = require('body-parser');
-const mongoose   = require('mongoose');
-const jwt        = require('jwt-simple');
-const cors       = require('cors');
+const express         = require('express');
+const app             = express();
+const bodyParser      = require('body-parser');
+const mongoose        = require('mongoose');
+const jwt             = require('jwt-simple');
+const cors            = require('cors');
+const path            = require('path'); 
+const crypto          = require('crypto'); 
+const multer          = require('multer');
+const GridFsStorage   = require('multer-gridfs-storage');
+const Grid            = require('gridfs-stream');
+const methodOverride  = require('method-override'); // Not necessary but maybe useful later on
 
 // -----------------------------------------------------------------------------------------
 // Internal Dependencies
@@ -20,11 +26,59 @@ const keys = require('./config/keys');
 // -----------------------------------------------------------------------------------------
 app.use(bodyParser.json({ type: '*/*' }));
 app.use(cors());
+app.use(methodOverride('_method')); 
 
 // -----------------------------------------------------------------------------------------
 // MongoDB Setup
 // -----------------------------------------------------------------------------------------
 mongoose.connect('mongodb://localhost:27017/imagestore', { useNewUrlParser: true, useUnifiedTopology: true });
+const conn = mongoose.connection;
+
+// -----------------------------------------------------------------------------------------
+// Initialize gfs (grid fs stream)
+// -----------------------------------------------------------------------------------------
+let gfs;
+
+conn.once('open', () => {
+    // Init stream
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+});
+
+// -----------------------------------------------------------------------------------------
+// Create storage engine
+// -----------------------------------------------------------------------------------------
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        } 
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads' // bucketname should match mongo collection name
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+
+const upload = multer({ 
+  storage: storage,
+  fileFilter: (req, file, cb) => { // Makes sure only images upload to the uploads collection
+      const ifValidFile = ['.png', '.jpg', '.jpeg'].some(ext => ext == path.extname(file.originalname));
+      
+      if(ifValidFile) {
+          return cb(null, true);
+      } else {
+          return cb(null, false);
+      }
+    }
+});
 
 // -----------------------------------------------------------------------------------------
 // Authentication API
