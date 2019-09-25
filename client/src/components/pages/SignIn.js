a1aaeee53f0d4944071dac9f0491415f941e311b
ericsei <ericsei@dell.home>
2019-09-25 14:40:21 -0400

    add Sign In & Sign Up pages

--- client/src/components/pages/SignIn.js
+++ client/src/components/pages/SignIn.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const SignIn = () => {
-    return (
-        <div>
-            <h1>This is Sign In Page. </h1>
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  });
+  const { email, password } = user;
+  const onChange = event => {
+    setUser({ [event.target.name]: event.target.value });
+  };
+  return (
+    <div className='form-container'>
+      <h1>Account Sign In </h1>
+      <form>
+        <div className='form-group'>
+          <label htmlFor='name'>Email</label>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            required
+            onChange={onChange}
+          ></input>
         </div>
-    )
-}
+        <div className='form-group'>
+          <label htmlFor='name'>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            required
+          ></input>
+        </div>
+        <input
+          type='submit'
+          value='Sign In'
+          className='btn btn-success btn-block'
+        ></input>
+      </form>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
