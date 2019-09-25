c3c22fd83efddc70bd8ccc08b1708f9c490f727b
ericsei <ericsei@dell.home>
2019-09-25 14:46:39 -0400

    update Sign In

--- client/src/components/pages/SignIn.js
+++ client/src/components/pages/SignIn.js
@@ -7,12 +7,23 @@ const SignIn = () => {
   });
   const { email, password } = user;
   const onChange = event => {
-    setUser({ [event.target.name]: event.target.value });
+    setUser({ ...user, [event.target.name]: event.target.value });
   };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (email === '' || password === '') {
+      alert('Please enter all field', 'danger');
+    } else {
+      // Sign In API
+      alert('Sign In !!');
+    }
+  };
+
   return (
     <div className='form-container'>
       <h1>Account Sign In </h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label htmlFor='name'>Email</label>
           <input
