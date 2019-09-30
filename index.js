369cfdbc058492a67fc739b6737dd99f89e4b26f
yongchengzhu <yongchengzhu002@gmail.com>
2019-09-30 18:14:51 -0400

    Moved display routes to index.js

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ const https           = require('https');
 // Internal Dependencies
 // -----------------------------------------------------------------------------------------
 if(process.env.NODE_ENV !== 'production') require('dotenv/config');
-const authRouter   = require('./routes/authentication');
-const uploadRouter = require('./routes/upload');
+const authRouter    = require('./routes/authentication');
+const uploadRouter  = require('./routes/upload');
 const displayRouter = require('./routes/display')
-const keys         = require('./config/keys');
+const keys          = require('./config/keys');
+const Image         = require('./models/image');
 
 // -----------------------------------------------------------------------------------------
 // Middlewares
@@ -47,7 +48,7 @@ conn.once('open', () => {
     gfs = Grid(conn.db, mongoose.mongo);
     gfs.collection('uploads');
     // Display Route Handlers.
-    displayRouter(app, gfs);
+    // displayRouter(app, gfs);
 });
 
 // -----------------------------------------------------------------------------------------
@@ -59,6 +60,33 @@ uploadRouter(app);
 // For debugging purposes.
 app.get('/api/display', (req, res) => {
   res.send({ msg: 'This is the display route!'});
+});
+
+//This will send all images' info
+app.get('/api/display/fetchall', (req, res) => {
+  Image.find()
+  .then(imgs => {
+      res.json(imgs)
+  })
+  .catch(err => res.json({err: err}));
+});
+
+//This will send image stream
+app.get('/api/display/filestream/:filename', (req, res) => {
+  gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+      if(err) {
+          return res.status(404).json({err});
+      }
+      // Check if file
+      if(!file || file.length == 0) {
+          return res.status(404).json({
+              err: 'No file exists'
+          });
+      };
+      res.contentType =  file.contentType;
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+  });    
 });
 
 // -----------------------------------------------------------------------------------------
